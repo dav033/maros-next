547274889774fc84c5c654bbfea9af83a8be9ef0
david.theran03@gmail.com <david.theran03@gmail.com>
2025-12-11 00:24:42 -0500

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,8 @@ const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "../davComponents/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@dav033/dav-components/src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@dav033/dav-components/dist/**/*.{js,ts,jsx,tsx}",
   ],
 };
 
